74dfd5a18c55556e2f0fb84f70458e29d352fdbd
Muhammet Emin Parlak <muparlak5353@gmail.com>
2026-01-20 01:29:29 +0300

    refactoring2

--- ShepidiSoft/src/core/ShepidiSoft.Domain/Entities/Assignment.cs
+++ ShepidiSoft/src/core/ShepidiSoft.Domain/Entities/Assignment.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShepidiSoft.Domain.Entities.Common;
 
-namespace ShepidiSoft.Domain.Entities
+namespace ShepidiSoft.Domain.Entities;
+
+public sealed class Assignment : BaseEntity<int>
 {
-    internal class Assignment
-    {
-    }
+    public int MyProperty { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime? DueDate { get; set; } // Teslim tarihi (Opsiyonel olabilir)
+    public bool IsActive { get; set; } = true; // Aktif/Pasif yönetimi için
+
+    // Foreign Key: 1 Assignment -> 1 Course
+    public int CourseId { get; set; }
+
+    // Navigation Properties
+    public Course Course { get; set; } = null!;
+
+    // 1 Assignment -> N Submissions (Henüz AssignmentSubmission sınıfı yoksa burayı yorum satırı yapabilirsiniz)
+     public ICollection<AssignmentSubmission> Submissions { get; set; } = new List<AssignmentSubmission>();
 }
