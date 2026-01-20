bf2bd99f0a8d8f12de52b7d1aeb96406e254dd47
Suleyman Meral <mrlslymn02@gmail.com>
2026-01-20 21:52:24 +0300

    feat(domain): create DocumentTopic entity (#28)

--- ShepidiSoft/src/core/ShepidiSoft.Domain/Entities/Document.cs
+++ ShepidiSoft/src/core/ShepidiSoft.Domain/Entities/Document.cs
@@ -1,22 +1,17 @@
 ﻿using ShepidiSoft.Domain.Entities.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ShepidiSoft.Domain.Entities
+
+namespace ShepidiSoft.Domain.Entities;
+
+public sealed class Document : BaseEntity<int>
 {
-    public sealed class Document : BaseEntity<int>
-    {
-        public int DocumentTopicId { get; set; }
-        public string Title { get; set; } = null!;
-        public string? Description { get; set; }
-        public string FileUrl { get; set; } = null!;
-        public DateTime PublishedAt { get; set; }
-        public string UploadedByUserId { get; set; } = null!;
+    public int DocumentTopicId { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public string FileUrl { get; set; } = null!;
+    public DateTime PublishedAt { get; set; }
+    public string UploadedByUserId { get; set; } = null!;
 
-        //// Navigation Properties
-        //public DocumentTopic DocumentTopic { get; set; } = null!;
-    }
+    //// Navigation Properties
+    //public DocumentTopic DocumentTopic { get; set; } = null!;
 }
