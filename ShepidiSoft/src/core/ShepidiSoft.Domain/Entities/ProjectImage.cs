4d98bc7fcf25c8a6f1d801c6852dec2509c619cc
Muhammetemin-parlak <muparlak5353@gmail.com>
2026-01-20 19:54:06 +0300

    19 featdomain create projectimage entity (#25)

---
+++ ShepidiSoft/src/core/ShepidiSoft.Domain/Entities/ProjectImage.cs
@@ -0,0 +1,17 @@
+﻿using ShepidiSoft.Domain.Entities.Common;
+
+namespace ShepidiSoft.Domain.Entities;
+
+public sealed class ProjectImage : BaseEntity<int>
+{
+    public string ImageUrl { get; set; } = null!; // Görselin yolu/linki
+    public bool IsMain { get; set; } = false; // Ana/Kapak görseli mi?
+    public int OrderIndex { get; set; } // Gösterim sırası
+    public string? Description { get; set; } // Görsel altı açıklaması (Tooltip vs.)
+
+    // İlişkiler (Foreign Key)
+    public int ProjectId { get; set; }
+
+    // Navigation Property
+    public Project Project { get; set; } = null!;
+}
