931a30a8818114d90d62ce8e298d04410de5b968
Süleyman <115658595+suleymannmeral@users.noreply.github.com>
2026-01-20 21:52:59 +0300

    feat(domain): create DocumentTopic entity (#28) (#29)

---
+++ ShepidiSoft/src/core/ShepidiSoft.Domain/Entities/DocumentTopic.cs
@@ -0,0 +1,13 @@
+﻿
+using ShepidiSoft.Domain.Entities.Common;
+
+namespace ShepidiSoft.Domain.Entities;
+
+public sealed class DocumentTopic : BaseEntity<int>
+{
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+
+    // Navigation Properties
+    public ICollection<Document> Documents { get; set; } = new List<Document>();
+}
